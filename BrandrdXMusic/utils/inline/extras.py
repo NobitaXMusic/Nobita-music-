e0c59815216924b2b255e325db3d947536f7c6be
NobitaXMusic <151773763+NobitaXMusic@users.noreply.github.com>
2023-12-23 21:55:14 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/Girls_and_boys_dpz_3"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
